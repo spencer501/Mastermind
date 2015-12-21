c0f0d34d35e0ce6040981342b328fbae50536429
Spencer <spencer.p.816@gmail.com>
2015-12-21 02:56:27 -0800

    Create initial java files
    Create files:
    - GameMain
    - CodeBreaker
    - CodeMaker

---
+++ src/com/spencerpease/mastermind/CodeMaker.java
@@ -0,0 +1,12 @@
+package com.spencerpease.mastermind;
+
+/**
+ * DESCRIPTION
+ * <p>
+ * Created by: Spencer Pease
+ * Created on: 2015-12-21
+ */
+public class CodeMaker {
+
+
+}
---
+++ src/com/spencerpease/mastermind/GameMain.java
@@ -0,0 +1,15 @@
+package com.spencerpease.mastermind;
+
+/**
+ * DESCRIPTION
+ * <p>
+ * Created by: Spencer Pease
+ * Created on: 2015-12-21
+ */
+public class GameMain {
+
+    public static void main(String[] args) {
+
+    }
+
+}
